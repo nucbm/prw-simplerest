eaa04f1b369e8a2977fdd33d43c52634527b626a
nucbm <stud.eeng@gmail.com>
2024-04-22 12:33:56 +0300

    Update todoListController.js

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,6 +1,12 @@
-'use strict';
- var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
+'use strict';
+var mongoose = require('mongoose'),
+    Task = mongoose.model('Tasks');
+
+
+
+/*
+
+MongooseError: Model.find() no longer accepts a callback
 
 exports.list_all_tasks = function(req, res) {
   Task.find({}, function(err, task) {
@@ -10,39 +16,68 @@ exports.list_all_tasks = function(req, res) {
   });
 };
 
-exports.create_a_task = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
+*/
+
+exports.list_all_tasks = async function(req, res) {
+    try {
+        // Așteaptă până când toate task-urile sunt găsite
+        const tasks = await Task.find({}); 
+        
+        // Trimite răspunsul JSON cu task-urile
+        res.json(tasks); 
+    } catch (err) {
+        res.status(500).send(err); // Trimite eroarea ca răspuns HTTP, cu cod de stare 500
+    }
 };
 
 
-exports.read_a_task = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
- 
-exports.update_a_task = function(req, res) {
-  Task.findOneAndUpdate({_id:req.params.taskId}, req.body, {new: true}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
+exports.create_a_task = async function(req, res) {
+    var new_task = new Task(req.body);
+    try {
+        const task = await new_task.save();
+        res.json(task);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
-// Task.remove({}).exec(function(){});
-exports.delete_a_task = function(req, res) {
-   Task.remove({
-    _id: req.params.taskId
-  }, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Task successfully deleted' });
-  });
+
+
+exports.read_a_task = async function(req, res) {
+    try {
+        const task = await Task.findById(req.params.taskId);
+        if (!task) {
+            return res.status(404).send({ message: 'Task not found' });
+        }
+        res.json(task);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+
+exports.update_a_task = async function(req, res) {
+    try {
+        const task = await Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true});
+        if (!task) {
+            return res.status(404).send({ message: 'Task not found' });
+        }
+        res.json(task);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+
+
+exports.delete_a_task = async function(req, res) {
+    try {
+        const result = await Task.deleteOne({ _id: req.params.taskId });
+        if (result.deletedCount === 0) {
+            return res.status(404).send({ message: 'Task not found' });
+        }
+        res.json({ message: 'Task successfully deleted' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
